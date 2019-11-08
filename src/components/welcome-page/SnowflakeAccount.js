d45e068d0198aaa07e78280bfbd34f6936fe743e
JanMooMoo <35936674+JanMooMoo@users.noreply.github.com>
2019-11-07 19:01:13 -0800

    commit

--- src/components/welcome-page/SnowflakeAccount.js
+++ src/components/welcome-page/SnowflakeAccount.js
@@ -270,7 +270,7 @@ componentWillUnmount(){
        <Row><Col><h1> </h1></Col></Row>
        <Row><Col><h1> </h1></Col></Row>
        
-  <Tabs defaultActiveKey="ethereum_deposit" transition={false} id="noanim-tab-example" mountOnEnter>
+  <Tabs defaultActiveKey="ein_sent" transition={false} id="noanim-tab-example" mountOnEnter>
   
   <Tab eventKey="resolver_added" title="Resolvers Added" className="tab">
    <ResolversAdded
